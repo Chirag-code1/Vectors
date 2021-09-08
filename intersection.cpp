14beef028e70001ed3d0030ccebffc7e8141e6d7
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-09-08 23:19:18 +0530

    Create intersection.cpp
    Write a function named intersection() that accepts
    
    references to two sorted Vector of integers as parameters
    and returns a new vector that contains only the elements that are found in both vectors. For example, if vector v1 and v2 store:
    
    v1 = {1, 2, 3, 4, 5, 6}
    
    v2 = {2, 4, 6, 8}
    
    Then the call of intersection(v1, v2) returns the vector: {2, 4, 6}

---
+++ intersection.cpp
@@ -0,0 +1,17 @@
+vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> result;
+        int l = 0, r = 0;
+        while(l < nums1.size() && r < nums2.size()){
+            int left = nums1[l], right = nums2[r];
+            if(left == right){
+                result.push_back(right);
+                while(l < nums1.size() && nums1[l] == left )l++;
+                while(r < nums2.size() && nums2[r] == right )r++;
+                continue;
+            }
+            if(left < right){
+            while(l < nums1.size() && nums1[l] == left )l++;
+            }else while( r < nums2.size()  && nums2[r] == right )r++;
+        }
+            return result;
+    }
